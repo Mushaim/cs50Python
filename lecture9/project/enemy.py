fa29b34cb7c10cd8009e958c0de3d471c99a7445
Mushaim Khan <69242014+Mushaim@users.noreply.github.com>
2025-09-07 03:14:49 +0500

    Delete lecture9/project directory

--- lecture9/project/enemy.py
+++
@@ -1,9 +0,0 @@
-from character import Character
-
-class Enemy(Character):
-    def __init__(self,name , health, attack, xp_reward, score_reward, loot=None):
-        super().__init__(name, health,attack)
-        self.xp_reward = xp_reward
-        self.score_reward = score_reward
-        self.loot = loot
-
